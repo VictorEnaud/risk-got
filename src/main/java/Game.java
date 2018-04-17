bc37ae9fa91ce3e69f3225aedf2cd9e573f89311
victorenaud <7222886+victorenaud@users.noreply.github.com>
2018-04-17 15:06:31 +0200

    Add kingdoms and players

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
+    private List<String> players;
+    private List<String> kingdoms;
+
+    public Game() {
+        this.players = new ArrayList<>();
+        this.players.add("Player 1");
+        this.players.add("Player 2");
+
+        this.kingdoms = new ArrayList<>();
+        this.kingdoms.add("Kingdom 1");
+        this.kingdoms.add("Kingdom 2");
+    }
+
+    public List<String> getPlayers() {
+        return players;
+    }
+
+    public List<String> getKingdoms() {
+        return kingdoms;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GameTest {
+    @Test
+    void shouldCreateTwoPlayers_onInit() {
+        // When
+        Game game = new Game();
 
+        // Then
+        assertThat(game.getPlayers()).hasSize(2);
+    }
+
+    @Test
+    void shouldCreateTwoKingdoms_onInit() {
+        // When
+        Game game = new Game();
+
+        // Then
+        assertThat(game.getKingdoms()).hasSize(2);
+    }
 }
